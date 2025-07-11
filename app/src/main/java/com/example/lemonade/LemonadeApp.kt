dd0ea598149ba508c6a063e864b5fea596c477d7
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-12 00:02:14 +0200

    wip: steps

--- app/src/main/java/com/example/lemonade/LemonadeApp.kt
+++ app/src/main/java/com/example/lemonade/LemonadeApp.kt
@@ -1,12 +1,13 @@
 package com.example.lemonade
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
@@ -20,10 +21,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LemonadeProcess(modifier: Modifier = Modifier) {
@@ -31,49 +34,39 @@ fun LemonadeProcess(modifier: Modifier = Modifier) {
 
     var currentStepIndex by remember { mutableIntStateOf(0) }
 
-    Button(
-        onClick = {
-            currentStepIndex = (currentStepIndex + 1) % steps.size
-        },
-        modifier = modifier.fillMaxWidth(),
-
-        ) {
-        val currentStep = steps[currentStepIndex]
-        Step(
-            modifier = modifier,
-            stepData = currentStep,
-        )
-    }
-}
-
-
-@Composable
-internal fun Step(
-    modifier: Modifier = Modifier,
-    stepData: StepData = LemonTreeStep,
-) {
     Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.Companion.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Image(
-            painter = painterResource(stepData.imageId),
-            contentDescription = stringResource(stepData.contentDescriptionId),
-            modifier = modifier.aspectRatio(1f).width(5.dp),
-            contentScale = ContentScale.Companion.Crop
+        val currentStep = steps[currentStepIndex]
+        Button(
+            onClick = { currentStepIndex = (currentStepIndex + 1) % steps.size },
+        ) {
+            Image(
+                painter = painterResource(currentStep.imageId),
+                contentDescription = stringResource(currentStep.contentDescriptionId),
+                modifier = modifier
+                    .width(120.dp)
+                    .background(colorResource(R.color.pale_green)),
+                contentScale = ContentScale.Crop,
 
-        )
-        Spacer(Modifier.Companion.height(16.dp))
+            )
+        }
+        Spacer(Modifier.height(16.dp))
         Text(
-            text = stringResource(stepData.textId),
-            color = Color.Companion.Black,
+            text = stringResource(currentStep.textId),
+            color = Color.Black,
+            fontSize = 18.sp,
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
-    LemonadeProcess(Modifier.Companion.fillMaxSize())
+    LemonadeProcess()
 }
